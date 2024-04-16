76cf0102204852bc24904671f29b88ea44c88255
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2024-04-16 14:10:01 +0300

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: "always",
+  useTabs: false,
+  tabWidth: 2,
+  endOfLine: "crlf",
+  trailingComma: "all",
+  singleQuote: true,
+  semi: false,
+  bracketSpacing: true,
+  printWidth: 80,
+  singleAttributePerLine: true,
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+}
---
+++ src/routing/AppRoute.ts
@@ -0,0 +1,7 @@
+export enum AppRoute {
+  HOME = '/',
+  MARKET = '/market',
+  MARKET_CATEGORY = '/market/:categoryId',
+  CAR_DETAILS = '/car/:carId',
+  ANY = '*',
+}

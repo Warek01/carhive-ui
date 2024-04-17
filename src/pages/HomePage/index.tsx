10c8d7c1f304c0555e73d5601a09bcfb92087da0
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2024-04-17 21:17:36 +0300

    chore(market category): remove market category link from home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,12 +1,8 @@
 import { FC } from 'react'
-import { Link } from 'react-router-dom'
-import { AppRoute } from '../../routing/AppRoute'
-import { generatePath } from 'react-router'
 
 const HomePage: FC = () => {
   return (
     <div>
-      <Link to={generatePath(AppRoute.MARKET_CATEGORY, { categoryId: '123124132' })}>Category</Link>
       <h1>Home</h1>
     </div>
   )

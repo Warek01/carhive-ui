999989a0a6d91ee978336bd9e3d7dd122372f4f5
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2024-04-22 23:20:43 +0300

    chore: add husky and upgrade node types

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,10 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['src/**/*.{ts,tsx,scss}'],
   darkMode: ['class'],
+  blocklist: ['node_modules/'],
+  important: false,
+  plugins: [],
   theme: {
     extend: {},
   },
-  plugins: [],
 } as Config
-

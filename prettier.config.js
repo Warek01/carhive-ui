cae7cce7610d80ffcf40fb1220f63133948318ae
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2024-07-10 00:08:16 +0300

    dev: reformat prettier config to js

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'always',
+  useTabs: false,
+  tabWidth: 2,
+  endOfLine: 'crlf',
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  bracketSpacing: true,
+  printWidth: 80,
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+};
+
+export default config;
